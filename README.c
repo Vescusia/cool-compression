4135e3c9472f654b9366df862761138794ace2f3
vescusia <clemens.krueger@campus.tu-berlin.de>
2026-09-09 14:52:29 +0200

    anton3: add README

---
+++ README.c
@@ -0,0 +1,10 @@
+# Install
+hehe - no
+
+# Usage
+does not work
+
+# Contributions
+When pushing to repo, play [this](https://www.youtube.com/watch?v=5nqIMN_5HIE) or it will be rejected.
+You will have to attach a video to the commit.
+Use CCP to compress it or it will be rejected.
